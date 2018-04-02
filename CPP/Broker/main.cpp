db5ec15a3407e2f99f18900da14a8fe36f688aa2
MaximilianPapa-SIP40 <papa@technikum-wien.at>
2018-04-02 09:35:16 +0000

    Added a few bomments for better understanding

--- CPP/Broker/main.cpp
+++ CPP/Broker/main.cpp
@@ -4,10 +4,13 @@
 #include <iostream>
 #include <sstream>
 
-void InitStation(std::string topic, std::string stationID);
-void InitMOR(std::string topic, std::string morID);
-void SendNewTaskToStation(std::string topic, std::string value);
-void AddTaskInMORQueue(std::string topic, std::string task);
+/*
+ * Prototypes of the callback functions for the broker
+ */
+void Callback_StationRegistration(std::string topic, std::string stationID);
+void Callback_MORRegistration(std::string topic, std::string morID);
+void Callback_StationTaskInProgress(std::string topic, std::string value);
+void Callback_NextTaskForMor(std::string topic, std::string task);
 void Callback_TakeTaskFromMOR(std::string topic, std::string taskID);
 
 TaskQueue 				m_TaskQueue;
@@ -15,25 +18,25 @@ MQTTCommunication 		m_mqttComm;
 std::vector<uint8_t>	m_MORList;
 	
 /*
- * Start here
+ * Entry point of the program
  */
 int main (int argc, char **argv)
 {	
-	INIReader reader("../../SIP40.ini");
-
-    if (reader.ParseError() < 0) {
-        std::cout << "Can't load 'SIP40.ini'\n";
-        return 1;
-    }
 	
-	std::string mqtt_Hostname = reader.Get("Broker", "MQTT_Hostname", "UNKNOWN");
-	int mqtt_Port = reader.GetInteger("Broker", "MQTT_Port", -1);
+	// Read the Ini-File
+	INIReader reader("../../SIP40.ini");
+	if (reader.ParseError() < 0) {
+		std::cout << "Can't load 'SIP40.ini'\n";
+		return 1;
+	}
+	std::string mqtt_Hostname 	= reader.Get("Broker", "MQTT_Hostname", "UNKNOWN");
+	int 		mqtt_Port 		= reader.GetInteger("Broker", "MQTT_Port", -1);
 	
 	if(m_mqttComm.Connect(mqtt_Hostname, mqtt_Port))
 	{
 		//m_mqttComm.Subscribe("SIP40_Factory/MOR_General/GetInitTaskListFromRobot")
-		m_mqttComm.Subscribe("SIP40_Factory/Anmeldung/Station", InitStation);
-		m_mqttComm.Subscribe("SIP40_Factory/Anmeldung/MOR", InitMOR);
+		m_mqttComm.Subscribe("SIP40_Factory/Anmeldung/Station", Callback_StationRegistration);
+		m_mqttComm.Subscribe("SIP40_Factory/Anmeldung/MOR", Callback_MORRegistration);
 		
 		while(1)
 		{
@@ -44,15 +47,33 @@ int main (int argc, char **argv)
 	return 0;
 }
 
-void InitStation(std::string topic, std::string stationID)
+/**
+ * Callback function, if a station registers
+ * 
+ * @detail		Send the station a task and subscribe to important topics
+ * @param [in]	topic 		- Topic of the received message
+ * @param [in]	stationID 	- Payload of the received message
+ * @return 		Nothing
+ */
+void Callback_StationRegistration(std::string topic, std::string stationID)
 {
 	m_mqttComm.Publish("SIP40_Factory/" + stationID + "/TaskForStation", "1");
-	m_mqttComm.Subscribe("SIP40_Factory/" + stationID + "/NextTaskForMOR", AddTaskInMORQueue);
-	m_mqttComm.Subscribe("SIP40_Factory/" + stationID + "/TaskInProgress", SendNewTaskToStation);
+	m_mqttComm.Subscribe("SIP40_Factory/" + stationID + "/NextTaskForMOR", Callback_NextTaskForMor);
+	m_mqttComm.Subscribe("SIP40_Factory/" + stationID + "/TaskInProgress", Callback_StationTaskInProgress);
     std::cout << "Sent on: SIP40_Factory/" << stationID << "/TaskForStation" << std::endl;
 }
 
-void InitMOR(std::string topic, std::string morID)
+/**
+ * Callback function, if a mobile robot registers
+ * 
+ * @detail		Create and save a unique identity for the new robot
+ * 				Send the mor this identity and the whole tasklist
+				Subscribe to important topics from this mobile robot
+ * @param [in]	topic 		- Topic of the received message
+ * @param [in]	morID 		- Payload of the received message
+ * @return 		Nothing
+ */
+void Callback_MORRegistration(std::string topic, std::string morID)
 {
 	morID = std::to_string(m_MORList.size());
 	m_MORList.push_back(stoi(morID));
@@ -65,7 +86,16 @@ void InitMOR(std::string topic, std::string morID)
     std::cout << "Sent on: SIP40_Factory/MOR_General/TaskQueue" << std::endl;
 }
 
-void SendNewTaskToStation(std::string topic, std::string value)
+/**
+ * Callback function, if a station changes his task progress
+ * 
+ * @detail		Parse the station name out of the topic
+ * 				If the station finished a task, send it a new one
+ * @param [in]	topic 		- Topic of the received message
+ * @param [in]	value 		- Payload of the received message (0 - task finished)
+ * @return 		Nothing
+ */
+void Callback_StationTaskInProgress(std::string topic, std::string value)
 {
 	std::string stationName;
 	std::istringstream issTaskQueue(topic);
@@ -79,12 +109,33 @@ void SendNewTaskToStation(std::string topic, std::string value)
 	}
 }
 
-void AddTaskInMORQueue(std::string topic, std::string task)
+/**
+ * Callback function, if a station has a new task for the mobile robot
+ * 
+ * @detail		Insert this new task in the global task queue
+ * 				Send the new current task queue to all mobile robots
+ * @param [in]	topic 		- Topic of the received message
+ * @param [in]	value 		- Payload of the received message (new Task)
+ * @return 		Nothing
+ */
+void Callback_NextTaskForMor(std::string topic, std::string task)
 {
 	m_TaskQueue.InsertNewTaskInQueueFromMQTTString(task);
 	m_mqttComm.Publish("SIP40_Factory/MOR_General/TaskQueue", m_TaskQueue.GetMQTTStringOfTaskQueue());
 }
 
+/**
+ * Callback function, if a mobile robot chooses a task to do
+ * 
+ * @detail		Extract the mobile robot identification out of the topic
+ * 				Check if the requested task exists or if another mobile robot took it already
+ * 				Response to the right mobile robot the state of the requested tast
+ * 				Delete the task if it is available and the mobile robot took it
+ * 				Send all mobile robots the new updated task queue
+ * @param [in]	topic 		- Topic of the received message
+ * @param [in]	value 		- Payload of the received message (TaskID of the requested task)
+ * @return 		Nothing
+ */
 void Callback_TakeTaskFromMOR(std::string topic, std::string taskID)
 {
 	std::string morName;
